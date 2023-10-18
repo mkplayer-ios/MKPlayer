8d3632ff99fd61bcd8e30ff570d040170555c3c0
Karthi K <karthi.k@mediakind.com>
2023-10-18 11:14:32 +0530

    Added 1.10.0 mkplayer

--- Package.swift
+++ Package.swift
@@ -30,6 +30,6 @@ let package = Package(
                     .define("BUILD_LIBRARY_FOR_DISTRIBUTION", to: "YES")
                 ]
         ),
-        .binaryTarget(name:"MKPlayer" , url: "#ZIPURL#", checksum: "#CHECKSUM#")
+        .binaryTarget(name:"MKPlayer" , url: "https://mkplayer.z13.web.core.windows.net/ios_tvos/MKPlayer-1.10.0.zip", checksum: "775c56271751b30a1aa613dc27dd3713a5dbd20b0e21ee3eddddcdcad091af53")
     ]
 )
