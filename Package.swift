f7b70426c269fcb19f38e6e7c562605dc61e1047
Karthi K <karthi.k@mediakind.com>
2023-11-14 04:50:07 +0530

    Added visionOS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "MKPlayer",
     platforms: [
         .iOS(.v14),
-        .tvOS(.v14)
+        .tvOS(.v14),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -17,19 +18,20 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.47.0"),
+                    exact:"3.49.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.0")
     ],
     
     targets: [
         .target(name: "MKPlayerPackage",
-                dependencies: [ .product(name: "BitmovinPlayer", package: "player-ios"),
-                                .product(name: "Alamofire", package: "Alamofire")
+                dependencies: [
+                    .product(name: "BitmovinPlayer", package: "player-ios"),
+                    .product(name: "Alamofire", package: "Alamofire")
                     ],
                 cSettings: [
                     .define("BUILD_LIBRARY_FOR_DISTRIBUTION", to: "YES")
                 ]
         ),
-        .binaryTarget(name:"MKPlayer" , url: "#ZIPURL#", checksum: "#CHECKSUM#")
+        .binaryTarget(name:"MKPlayer" , url: "https://mkplayer.blob.core.windows.net/$web/ios_tvos/MKPlayer-1.11.0-rc4.zip", checksum: "ed1a77734a7a04e4abdaa2b3ccaea98ecdd3ffaba632f2f32452ddc84a295705")
     ]
 )
