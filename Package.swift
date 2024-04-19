3b7528facd0ce27a763509d5a36d29a91599b743
johnikarthi <johnikarthi@gmail.com>
2024-04-19 11:32:36 +0530

    Update Package.swift
    Updated Bitmovin version to 3.54.0
    Updated Alamofire version to 5.9.0

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.53.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.0")
+                    exact:"3.54.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.0")
     ],
     
     targets: [
