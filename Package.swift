24e24cb5d1446ab3f885c5375c02cb8c8d9438fd
johnikarthi <johnikarthi@gmail.com>
2024-05-16 13:11:41 +0530

    Update Package.swift
    Reverted last changes

--- Package.swift
+++ Package.swift
@@ -18,12 +18,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.54.0")
+                    exact:"3.54.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.0")
     ],
     
     targets: [
         .target(name: "MKPlayerPackage",
-                dependencies: [ .product(name: "BitmovinPlayer", package: "player-ios") ],
+                dependencies: [ 
+                        .product(name: "BitmovinPlayer", package: "player-ios"),
+                        .product(name: "Alamofire", package: "Alamofire")
+                ],
                 cSettings: [
                     .define("BUILD_LIBRARY_FOR_DISTRIBUTION", to: "YES")
                 ]
