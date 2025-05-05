c075140ff13695dc8f3d46841891e2af4478d5bc
svc-mediakind <support@mediakind.com>
2025-05-05 14:15:10 +0530

    Updated url and checksum

--- Package.swift
+++ Package.swift
@@ -30,6 +30,6 @@ let package = Package(
                     .define("BUILD_LIBRARY_FOR_DISTRIBUTION", to: "YES")
                 ]
         ),
-        .binaryTarget(name:"MKPlayer" , url: "#ZIPURL#", checksum: "#CHECKSUM#")
+        .binaryTarget(name:"MKPlayer" , url: "https://mkplayer.z13.web.core.windows.net/ios_tvos/MKPlayer-1.39.0-PL.zip", checksum: "97117c3615b21c28173b7bf2d1fa1ff70b12fe433982875e013e31c4755833ab")
     ]
 )
