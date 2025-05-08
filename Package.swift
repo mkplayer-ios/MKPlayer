4602c8dfdd1a4e0fc36048be5d2ed23e540a7bb7
bharat <bharat@Bharaths-M3-Pro.local>
2025-05-08 13:20:00 +0530

    updating Bitmovin version to 3.85.2

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.87.0")
+                    exact:"3.85.2")
     ],
     
     targets: [
