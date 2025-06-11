330505a74b44dbf62389e94274d1eb779aa4ea02
MKA-9022 <mka-9022@Admins-MacBook-Pro-8.local>
2025-06-11 11:54:59 +0530

    updating Bitmovin player version to 3.90.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.90.1")
+                    exact:"3.90.0")
     ],
     
     targets: [
