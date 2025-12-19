69b84458dcbe75cdfb60811ba08a99f139363aca
johnikarthi <johnikarthi@gmail.com>
2025-12-19 16:14:38 +0530

    Update Package.swift
    Updated Bitmovin version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.97.1")
+                    exact:"3.99.0")
     ],
     
     targets: [
