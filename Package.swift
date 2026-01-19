e35a577492b4e3e640492426b29604ec334dd6c1
MKA-9022 <mka-9022@MKA-9022.local>
2026-01-19 18:15:19 +0530

    Updating Bitmovin version to 3.104.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/bitmovin/player-ios.git",
-                    exact:"3.99.0")
+                    exact:"3.104.0")
     ],
     
     targets: [
